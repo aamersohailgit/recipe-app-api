c3f227fedac22a3366c455523cfc1cd3d10d6353
aamersohailgit <84438123+aamersohailgit@users.noreply.github.com>
2023-05-28 18:46:00 +1000

    project updated

--- .app/app/settings.py
+++ .app/app/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-mp1vw%=#(pcje7u5qm1v&(d77s+lobj8fj$518@*5u#aryk@!!'
+SECRET_KEY = 'django-insecure-y^%34d2@zs0m#%ta*!@i9-8v47h(k5_iaj5vibym7h_9i++dg%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
